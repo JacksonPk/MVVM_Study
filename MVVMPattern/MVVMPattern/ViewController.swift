bf3d905162dc6f72692fe6fbe7d00dc08bfb7786
JinseoPark <rarebook92@naver.com>
2022-01-29 13:55:15 +0900

    Create UI frame

--- MVVMPattern/MVVMPattern/ViewController.swift
+++ MVVMPattern/MVVMPattern/ViewController.swift
@@ -9,60 +9,46 @@ import UIKit
 import SnapKit
 import Alamofire
 
-
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     /*
      Text 1개 -> 해당 화면에 나타날 값
         -년,월,일,시,분
      버튼 3개 -> 어제, 현재, 지금 버튼
      */
-    private lazy var yearLabel: UILabel = {
-        let label = UILabel()
-        label.text = "년"
-        label.font.withSize(10)
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private lazy var monthLabel: UILabel = {
-        let label = UILabel()
-        label.text = "월"
-        label.font.withSize(10)
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private lazy var dayLabel: UILabel = {
-        let label = UILabel()
-        label.text = "일"
-        label.font.withSize(10)
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private lazy var hourLabel: UILabel = {
-        let label = UILabel()
-        label.text = "시"
-        label.font.withSize(10)
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private lazy var minLabel: UILabel = {
-        let label = UILabel()
-        label.text = "분"
-        label.font.withSize(10)
-        label.textAlignment = .center
-        return label
-    }()
     
     private lazy var dateLabel: UILabel = {
         let label = UILabel()
-        label.text = "안녕하세요 잭슨입니당!! "
-//        label.tintColor = UIColorl
+        label.text = "2022년 2월 22일 2시 22분"
         label.font = UIFont.boldSystemFont(ofSize: 16)
         label.textAlignment = .center
         return label
+    }()
+    
+    private lazy var yesterdayBtn: UIButton = {
+       let button = UIButton()
+        button.setTitle("Yesterday", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 15)
+        button.addTarget(self, action: #selector(yesterDayBtnTouchedUpInside), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var todayBtn: UIButton = {
+       let button = UIButton()
+        button.setTitle("Today", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 15)
+        button.addTarget(self, action: #selector(todayBtnTouchedUpInside), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var tomorrowBtn: UIButton = {
+       let button = UIButton()
+        button.setTitle("Tomorrow", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 15)
+        button.addTarget(self, action: #selector(tommorowBtnTouchedUpInside), for: .touchUpInside)
+        return button
     }()
     
     override func viewDidLoad() {
@@ -72,8 +58,8 @@ class ViewController: UIViewController {
 
     private func configure() {
         self.view.backgroundColor = .white
-//        initTextView()
         addDateLabel()
+        addBtns()
     }
 
     private func addDateLabel() {
@@ -81,42 +67,45 @@ class ViewController: UIViewController {
         
         dateLabel.snp.makeConstraints { make -> Void in
             make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview().offset(-16)
+            make.centerY.equalToSuperview().offset(-64)
         }
     }
-
-    private func initTextView() {
-        view.addSubview(yearLabel)
-        view.addSubview(monthLabel)
-        view.addSubview(dayLabel)
-        view.addSubview(hourLabel)
-        view.addSubview(minLabel)
-        
-        yearLabel.snp.makeConstraints { label -> Void in
-            label.centerY.equalToSuperview()
-            label.leading.equalToSuperview().offset(100)
+    
+    private func addBtns() {
+        view.addSubview(todayBtn)
+        todayBtn.snp.makeConstraints { make -> Void in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(16)
         }
         
-        monthLabel.snp.makeConstraints { label -> Void in
-            label.centerY.equalToSuperview()
-            label.leading.equalTo(yearLabel.snp.trailing).offset(16)
+        view.addSubview(yesterdayBtn)
+        yesterdayBtn.snp.makeConstraints { make -> Void in
+            make.trailing.equalTo(todayBtn.snp.leading).offset(-42)
+            make.centerY.equalToSuperview().offset(16)
         }
-        
-        dayLabel.snp.makeConstraints { label -> Void in
-            label.centerY.equalToSuperview()
-            label.leading.equalTo(monthLabel.snp.trailing).offset(16)
+                
+        view.addSubview(tomorrowBtn)
+        tomorrowBtn.snp.makeConstraints { make -> Void in
+            make.leading.equalTo(todayBtn.snp.trailing).offset(42)
+            make.centerY.equalToSuperview().offset(16)
         }
-        
-        hourLabel.snp.makeConstraints { label -> Void in
-            label.centerY.equalToSuperview()
-            label.leading.equalTo(dayLabel.snp.trailing).offset(16)
-        }
-        
-        minLabel.snp.makeConstraints { label -> Void in
-            label.centerY.equalToSuperview()
-            label.leading.equalTo(hourLabel.snp.trailing).offset(16)
-        }
+    }
+    
+    private func setDateText() {
         
     }
+    
+    @objc private func yesterDayBtnTouchedUpInside() {
+        print("YesterDay")
+    }
+    
+    @objc private func todayBtnTouchedUpInside() {
+        print("Today")
+    }
+    
+    @objc private func tommorowBtnTouchedUpInside() {
+        print("Tommorow")
+    }
+    
 }
 
